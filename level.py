898553accb53de00d6f2d7d01a65aff1fb580141
ONKJUNUA <jaseko2002@gmail.com>
2022-07-11 21:44:14 +0200

    moving player

--- level.py
+++ level.py
@@ -21,7 +21,8 @@ class Level:
                 if col == 'x':
                     Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
                 if col == 'p':
-                    Player((x,y),[self.visible_sprites])
+                    self.player = Player((x,y),[self.visible_sprites])
 
     def run(self):
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
